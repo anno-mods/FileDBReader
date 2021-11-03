221f636351cb5384ac27e0cd04e5e623bc0b74ce
montag.lennard@gmail.com <montag.lennard@gmail.com>
2021-11-03 20:55:50 +0100

    Remove unused syroot stuff

--- src/FcFiles/BinaryExtensions.cs
+++ src/FcFiles/BinaryExtensions.cs
@@ -1,6 +1,4 @@
-﻿using Syroot.BinaryData.Core;
-using Syroot.BinaryData.Memory;
-using System;
+﻿using System;
 using System.IO;
 using System.Text;
 
@@ -9,15 +7,6 @@ namespace FileDBReader {
   public static class BinaryExtensions {
 
     #region Methods
-
-    public static SpanReader ToReader(this ReadOnlySpan<byte> span, Endian endian = Endian.System, Encoding encoding = null) {
-      return new SpanReader(span, endian, encoding);
-    }
-
-    public static SpanReader ToReader(this ReadOnlySpan<byte> span, out SpanReader reader, Endian endian = Endian.System, Encoding encoding = null) {
-      reader = new SpanReader(span, endian, encoding);
-      return reader;
-    }
 
     public static string ToHexString(this ReadOnlySpan<byte> Bytes) {
       StringBuilder Result = new StringBuilder(Bytes.Length * 2);
