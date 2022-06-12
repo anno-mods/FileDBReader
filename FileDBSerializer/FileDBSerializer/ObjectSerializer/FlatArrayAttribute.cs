f22aa56a2f90129a10967de7d4bee13f12560e45
taubenangriff <montag.lennard@gmail.com>
2022-06-12 23:11:19 +0200

    Add property attribute for flat arrays

---
+++ FileDBSerializer/FileDBSerializer/ObjectSerializer/FlatArrayAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDBSerializing.ObjectSerializer
+{
+    //Set this attribute to achieve arrays without a wrapper around them
+    [AttributeUsage(AttributeTargets.Property)]
+    public class FlatArrayAttribute : Attribute
+    {
+        private bool _isFlat;
+
+        public virtual bool IsFlatArray {
+            get => _isFlat;
+        }
+        public FlatArrayAttribute(bool value)
+        {
+            _isFlat = value;
+        }
+
+        public FlatArrayAttribute()
+        {
+            _isFlat = true;
+        }
+
+    }
+}
